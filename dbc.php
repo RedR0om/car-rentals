fbb9c4f112dc90696de8b0e775654be963c6e597
kelvindichoso <kelvind@apc.edu.ph>
2025-03-14 16:27:46 +0800

    additional configuration

--- dbc.php
+++ dbc.php
@@ -1,7 +1,7 @@
 <?php
 //database connection
 try {
-$dbc = new PDO('mysql:host=localhost; dbname=carrental', 'root', '');
+$dbc = new PDO('mysql:host=ballast.proxy.rlwy.net:35637; dbname=carrental', 'root', 'BobDdBAPBobrKyzYicQYaJhDpujZqoKa');
 $dbc->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch(PDOException $e) {
 	echo "Error: " . $e->getMessage();
