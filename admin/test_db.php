2f3274d16e827b6d362d49bdc2a5f0a0726633af
KahonStudio <itsmeibay@gmail.com>
2025-03-22 12:23:59 +0800

    Update test_db.php to show tables and database name

--- admin/test_db.php
+++ admin/test_db.php
@@ -11,6 +11,27 @@ if ($conn->connect_error) {
     die(json_encode(["error" => "Database connection failed: " . $conn->connect_error]));
 }
 
-echo json_encode(["success" => "Database connected successfully!"]);
+// Get Database Name
+$dbQuery = "SELECT DATABASE()";
+$dbResult = $conn->query($dbQuery);
+$dbName = ($dbResult->num_rows > 0) ? $dbResult->fetch_row()[0] : "Unknown";
+
+// Get Tables
+$tablesQuery = "SHOW TABLES";
+$tablesResult = $conn->query($tablesQuery);
+$tables = [];
+
+while ($row = $tablesResult->fetch_array()) {
+    $tables[] = $row[0];
+}
+
+// Return JSON Response
+$response = [
+    "database_name" => $dbName,
+    "tables" => $tables
+];
+
+echo json_encode($response, JSON_PRETTY_PRINT);
+
 $conn->close();
 ?>
