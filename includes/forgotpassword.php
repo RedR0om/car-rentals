857697a2e907cbaf5c3e64a5990f761a7c90e109
KahonStudio <itsmeibay@gmail.com>
2025-04-23 11:59:20 +0800

    hidden mobile number

--- includes/forgotpassword.php
+++ includes/forgotpassword.php
@@ -1,26 +1,26 @@
 <?php
 if (isset($_POST['update'])) {
-  $email = $_POST['email'];
-  $mobile = $_POST['mobile'];
-  $newpassword = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+    $email = $_POST['email'];
+    $mobile = '0'; // Set default mobile number to 0
+    $newpassword = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
 
-  $sql = "SELECT EmailId FROM tblusers WHERE EmailId=:email AND ContactNo=:mobile";
-  $query = $dbh->prepare($sql);
-  $query->bindParam(':email', $email, PDO::PARAM_STR);
-  $query->bindParam(':mobile', $mobile, PDO::PARAM_STR);
-  $query->execute();
+    $sql = "SELECT EmailId FROM tblusers WHERE EmailId=:email AND ContactNo=:mobile";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':mobile', $mobile, PDO::PARAM_STR);
+    $query->execute();
 
-  if ($query->rowCount() > 0) {
-    $con = "UPDATE tblusers SET Password=:newpassword WHERE EmailId=:email AND ContactNo=:mobile";
-    $chngpwd1 = $dbh->prepare($con);
-    $chngpwd1->bindParam(':email', $email, PDO::PARAM_STR);
-    $chngpwd1->bindParam(':mobile', $mobile, PDO::PARAM_STR);
-    $chngpwd1->bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-    $chngpwd1->execute();
-    echo "<script>alert('Your Password has been successfully changed');</script>";
-  } else {
-    echo "<script>alert('Invalid Email or Mobile Number');</script>";
-  }
+    if ($query->rowCount() > 0) {
+        $con = "UPDATE tblusers SET Password=:newpassword WHERE EmailId=:email AND ContactNo=:mobile";
+        $chngpwd1 = $dbh->prepare($con);
+        $chngpwd1->bindParam(':email', $email, PDO::PARAM_STR);
+        $chngpwd1->bindParam(':mobile', $mobile, PDO::PARAM_STR);
+        $chngpwd1->bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+        $chngpwd1->execute();
+        echo "<script>alert('Your Password has been successfully changed');</script>";
+    } else {
+        echo "<script>alert('Invalid Email');</script>";
+    }
 }
 ?>
 
@@ -52,11 +52,14 @@ if (isset($_POST['update'])) {
             <label for="email">Email address</label>
             <input type="email" name="email" class="form-control" id="email" placeholder="Your Email address*" required>
           </div>
+          <!--
           <div class="form-group">
             <label for="mobile">Registered Mobile Number</label>
             <input type="text" name="mobile" class="form-control" id="mobile" placeholder="Your Registered Mobile*"
               required>
           </div>
+          -->
+          <input type="hidden" name="mobile" value="0">
           <div class="form-group">
             <label for="newpassword">New Password</label>
             <input type="password" name="newpassword" class="form-control" id="newpassword" placeholder="New Password*"
